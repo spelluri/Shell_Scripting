08ed5a4ce474059a11686eb035d6a076829fd61d
spelluri <48129105+spelluri@users.noreply.github.com>
2024-03-26 06:01:49 +0100

    Create variable.sh

---
+++ variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Declaration of variables 
+
+name='shraddha'
+age=34
+today='tuesday'
+website='killercoda1.com'
+echo "My Name is :"$name
+echo "Age:"$age
+echo "$today is a very bright day"
+echo "I am using $website as a ubuntu playground"
