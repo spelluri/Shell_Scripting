7b6d63288710afbdde256eb8e413d948feb45e99
spelluri <48129105+spelluri@users.noreply.github.com>
2024-03-26 06:39:49 +0100

    Create user_input1.sh

---
+++ user_input1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#This script reads user input with prompt message "
+read -p "Enter the number:" num 
+echo "The number is: " $num
+
+read -p "please enter name:" name 
+echo "my name is:" $name
