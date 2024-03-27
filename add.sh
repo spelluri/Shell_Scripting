5903437e86ed4ccf57febebe85a1badd3b26a581
spelluri <48129105+spelluri@users.noreply.github.com>
2024-03-27 06:40:27 +0100

    Create add.sh

---
+++ add.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#This script showcases addition of numbers . 
+num1=$1 
+num2=$2
+echo "Value of num1 is:"$num1
+echo "value of num2 is:"$num2
+add=$(($num1+$num2))
+echo "Output is :" $add
+
+a=3
+b=7
+c=$(($a+$b))
+echo "output of c is :"$c
