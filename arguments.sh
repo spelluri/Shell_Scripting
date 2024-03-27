d9a1e5d1277c299ef9cf77e83bff219ad89a6631
spelluri <48129105+spelluri@users.noreply.github.com>
2024-03-27 06:41:38 +0100

    Create arguments.sh

---
+++ arguments.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+#This script showcases how passing values to variables as command line arguments works in shell scripting .
+
+today_date=$1
+current_year=$2
+echo "Today is $today_date and This is $current_year."
